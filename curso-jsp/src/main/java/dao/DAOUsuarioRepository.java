7cc32b18fa7b605bc8072bcf9b59d6a87d440ee7
Auri Martins <aurimsjr@gmail.com>
2022-07-19 13:08:22 -0300

    Criando comando SQL e método de consultar o usuário por login

--- curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
+++ curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import connection.SingleConnectionBanco;
 import model.ModelLogin;
@@ -14,7 +15,7 @@ public class DAOUsuarioRepository {
 		connection = SingleConnectionBanco.getConnection();
 	}
 	
-	public void gravarUsuario(ModelLogin objeto) throws Exception{
+	public ModelLogin gravarUsuario(ModelLogin objeto) throws Exception{
 		
 		String sql = "INSERT INTO public.model_login(login, senha, nome, email) VALUES (?, ?, ?, ?)";
 		PreparedStatement preparedSql = connection.prepareStatement(sql);
@@ -26,6 +27,30 @@ public class DAOUsuarioRepository {
 
 		preparedSql.execute();
 		connection.commit();
+		
+		return this.consultaUsuario(objeto.getLogin());//consulta usuario pelo login;
 	}
-
+	
+	
+	public ModelLogin consultaUsuario (String login) throws Exception {
+		
+		ModelLogin modelLogin =  new ModelLogin();
+		
+		String sql = "select * from model_login where upper (login) = upper ('"+login+"')";
+		
+		PreparedStatement statement = connection.prepareStatement(sql); // preparando SQL
+		
+		ResultSet resultado = statement.executeQuery();//executa a SQL
+		
+		while(resultado.next()) { /*Se tem resultado*/
+			
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setSenha(resultado.getString("senha"));
+			modelLogin.setNome(resultado.getString("nome"));
+		}
+		
+		return modelLogin;
+	}
 }
